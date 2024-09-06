29a57f946debba64191fb713b6d222cc59bf68dc
Lucas Iwanczuk <102380264+lucasiwanczuk@users.noreply.github.com>
2024-09-06 08:40:35 -0300

    Commit last Blitzcrank

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import csvParser from 'csv-parser';
 import dotenv from 'dotenv';
 import pkg from 'pg';
 import winston from 'winston';
+import { scheduleJob } from "node-schedule";
 dotenv.config();
 // USUARIO_INTRA="03244525008"
 // SENHA_INTRA="VK6-GA&)duW9YMK6"
@@ -17,169 +18,174 @@ const senha = "VK6-GA&)duW9YMK6";
 const hostdb = "177.126.159.93";
 const userdb = "postgres";
 const passdb = "123@mudar";
-
 const { Client } = pkg;
-
 const downloadPath = "./";
 const fileName = "export_file.csv";
 
 const logger = winston.createLogger({
-    level: 'info',
-    format: winston.format.combine(
-      winston.format.timestamp({
-        format: 'YYYY-MM-DD HH:mm:ss'
-      }),
-      winston.format.printf(({ timestamp, level, message }) => {
-        return `${timestamp} [${level.toUpperCase()}]: ${message}`;
-      })
-    ),
-    transports: [
-      new winston.transports.Console(),
-      // Você pode adicionar mais transportes como arquivo, etc.
-    ],
-  });
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss'
+    }),
+    winston.format.printf(({ timestamp, level, message }) => {
+      return `${timestamp} [${level.toUpperCase()}]: ${message}`;
+    })
+  ),
+  transports: [
+    new winston.transports.Console(),
+    // Você pode adicionar mais transportes como arquivo, etc.
+  ],
+});
 
-const client = new Client({
+// const client = new Client({
+//   user: userdb,
+//   host: hostdb,
+//   database: 'db_chamados',
+//   password: passdb,
+//   port: 8877, // ou a porta que você estiver usando
+// });
+
+const columnMapping = {
+  'Código': 'cod_protocol',
+  'Assunto': 'subject',
+  'Contato': 'contact',
+  'Email do Contato': 'contact_email',
+  'Unidade': 'unit',
+  'Departamento': 'department',
+  'Cargo': 'position',
+  'Tipo': 'type',
+  'Situação': 'status',
+  'Atendente': 'attendant',
+  'Grupo': 'group',
+  'Prioridade': 'priority',
+  'Criado Em': 'created_at',
+  'Prazo da primeira resposta': 'first_response_deadline',
+  'Primeira resposta': 'first_response',
+  'Prazo de resolução': 'resolution_deadline',
+  'Concluído Em': 'completed_at',
+  'Ultima alteração': 'last_modified',
+  'SLA': 'sla',
+  'Primeira atribuição em': 'first_assignment_at',
+  'Cancelado Em': 'canceled_at'
+};
+
+async function main() {
+
+  const client = new Client({
     user: userdb,
     host: hostdb,
     database: 'db_chamados',
     password: passdb,
     port: 8877, // ou a porta que você estiver usando
-});
+  });
 
-const columnMapping = {
-    'Código': 'cod_protocol',
-    'Assunto': 'subject',
-    'Contato': 'contact',
-    'Email do Contato': 'contact_email',
-    'Unidade': 'unit',
-    'Departamento': 'department',
-    'Cargo': 'position',
-    'Tipo': 'type',
-    'Situação': 'status',
-    'Atendente': 'attendant',
-    'Grupo': 'group',
-    'Prioridade': 'priority',
-    'Criado Em': 'created_at',
-    'Prazo da primeira resposta': 'first_response_deadline',
-    'Primeira resposta': 'first_response',
-    'Prazo de resolução': 'resolution_deadline',
-    'Concluído Em': 'completed_at',
-    'Ultima alteração': 'last_modified',
-    'SLA': 'sla',
-    'Primeira atribuição em': 'first_assignment_at',
-    'Cancelado Em': 'canceled_at'
-};
-async function main(){
+  // Inicia o Script
+  console.log('Iniciando o navegador...');
 
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
 
-// Inicia o Script
-console.log('Iniciando o navegador...');
+  const page = await browser.newPage();
 
-    const browser = await puppeteer.launch({
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    });
+  // ###################
+  const url = 'https://intra.mundovoa.com/admin/helpdesk/chamado/exportar';
+  console.log(`Acessando a URL: ${url}`);
+  await page.goto(url);
 
-const page = await browser.newPage();
+  // ###################
+  console.log(`Inserindo o nome de usuário: ${usuario}`);
+  await page.type('#username', usuario);
 
-// ###################
-const url = 'https://intra.mundovoa.com/admin/helpdesk/chamado/exportar';
-console.log(`Acessando a URL: ${url}`);
-await page.goto(url);
+  // ###################
+  console.log('Clicando no botão de acessar...');
+  await page.click('button[type="submit"]');
 
-// ###################
-console.log(`Inserindo o nome de usuário: ${usuario}`);
-await page.type('#username', usuario);
+  // ###################
+  console.log('Aguardando a página carregar...');
+  await page.waitForSelector('#password');
 
-// ###################
-console.log('Clicando no botão de acessar...');
-await page.click('button[type="submit"]');
+  // ###################
+  console.log('Inserindo a senha...');
+  await page.type('#password', senha);
 
-// ###################
-console.log('Aguardando a página carregar...');
-await page.waitForSelector('#password');
+  // ###################
+  console.log('Clicando no botão de entrar...');
+  await page.click('button[type="submit"]');
 
-// ###################
-console.log('Inserindo a senha...');
-await page.type('#password', senha);
+  // ###################
+  console.log('Aguardando a página carregar...');
+  await page.waitForSelector('input[name="confirm-password"]');
 
-// ###################
-console.log('Clicando no botão de entrar...');
-await page.click('button[type="submit"]');
+  // ###################
+  console.log(`Acessando a URL de export: ${url}`);
+  await page.goto(url);
 
-// ###################
-console.log('Aguardando a página carregar...');
-await page.waitForSelector('input[name="confirm-password"]');
+  // ###################
+  console.log('Inserindo a senha...');
+  await page.type('input[name="confirm-password"]', senha);
 
-// ###################
-console.log(`Acessando a URL de export: ${url}`);
-await page.goto(url);
+  // ###################
+  console.log('Clicando no botão de entrar...');
+  await page.click('button[type="submit"]');
 
-// ###################
-console.log('Inserindo a senha...');
-await page.type('input[name="confirm-password"]', senha);
+  // Agora que estamos autenticados, podemos capturar o link do arquivo
+  const downloadUrl = 'https://intra.mundovoa.com/admin/helpdesk/chamado/exportar'; // Link do arquivo
+  console.log(`Capturando o link de download: ${downloadUrl}`);
 
-// ###################
-console.log('Clicando no botão de entrar...');
-await page.click('button[type="submit"]');
-
-// Agora que estamos autenticados, podemos capturar o link do arquivo
-const downloadUrl = 'https://intra.mundovoa.com/admin/helpdesk/chamado/exportar'; // Link do arquivo
-console.log(`Capturando o link de download: ${downloadUrl}`);
-
-// Utilizando node-fetch para realizar o download diretamente após a autenticação
-const response = await fetch(downloadUrl, {
+  // Utilizando node-fetch para realizar o download diretamente após a autenticação
+  const response = await fetch(downloadUrl, {
     headers: {
-        'cookie': await page.cookies().then(cookies => cookies.map(cookie => `${cookie.name}=${cookie.value}`).join('; ')),
+      'cookie': await page.cookies().then(cookies => cookies.map(cookie => `${cookie.name}=${cookie.value}`).join('; ')),
     },
-});
+  });
 
-if (!response.ok) throw new Error(`Erro ao baixar o arquivo: ${response.statusText}`);
+  if (!response.ok) throw new Error(`Erro ao baixar o arquivo: ${response.statusText}`);
 
-// Cria o stream de escrita para salvar o arquivo na pasta definida
-const fileStream = fs.createWriteStream(path.join(downloadPath, fileName));
-console.log('Baixando o arquivo...');
+  // Cria o stream de escrita para salvar o arquivo na pasta definida
+  const fileStream = fs.createWriteStream(path.join(downloadPath, fileName));
+  console.log('Baixando o arquivo...');
 
-// Salva o arquivo binário
-response.body.pipe(fileStream);
-response.body.on('error', (err) => {
+  // Salva o arquivo binário
+  response.body.pipe(fileStream);
+  response.body.on('error', (err) => {
     console.error('Erro ao salvar o arquivo:', err);
-});
+  });
 
-fileStream.on('finish', () => {
+  fileStream.on('finish', () => {
     console.log('Arquivo baixado com sucesso!');
-});
+  });
 
-// Fecha o navegador após o download
-await browser.close();
+  // Fecha o navegador após o download
+  await browser.close();
 
-// Conecte-se ao banco de dados
-await client.connect();
+  // Conecte-se ao banco de dados
+  await client.connect();
 
-// Caminho para o arquivo CSV
-const filePath = path.join(downloadPath, fileName);
+  // Caminho para o arquivo CSV
+  const filePath = path.join(downloadPath, fileName);
 
-// Função para processar o CSV e inserir dados no banco
-async function processCSV() {
+  // Função para processar o CSV e inserir dados no banco
+  async function processCSV() {
     return new Promise((resolve, reject) => {
-        const results = [];
+      const results = [];
 
-        fs.createReadStream(filePath)
-            .pipe(csvParser({ separator: ';' }))  // Especifica o delimitador como ponto e vírgula
-            .on('data', (data) => {
-                // console.log('Dados lidos:', data); // Log dos dados lidos
-                results.push(data);
-            })
-            .on('end', async () => {
-                try {
-                    for (const row of results) {
-                        console.log(row)
+      fs.createReadStream(filePath)
+        .pipe(csvParser({ separator: ';' }))  // Especifica o delimitador como ponto e vírgula
+        .on('data', (data) => {
+          // console.log('Dados lidos:', data); // Log dos dados lidos
+          results.push(data);
+        })
+        .on('end', async () => {
+          try {
+            for (const row of results) {
+              console.log(row)
 
-                        const values = Object.values(row)
-
-                        // Execute the insertion
-                        console.log('Valores a serem inseridos:', values);  // Verifique se o email está correto aqui
-                        await client.query(`
+              const values = Object.values(row)
+              // Execute the insertion
+              console.log('Valores a serem inseridos:', values);  // Verifique se o email está correto aqui
+              await client.query(`
                         INSERT INTO int_chamados_ti ("cod_protocol", "subject", "contact", "contact_email", "unit", "department", "position", "type", "status", "attendant", "group", "priority", "created_at", "first_response_deadline", "first_response", "resolution_deadline", "completed_at", "last_modified", "sla", "first_assignment_at", "canceled_at")
                         VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21)
                         ON CONFLICT ("cod_protocol")
@@ -205,28 +211,28 @@ async function processCSV() {
                             "first_assignment_at" = EXCLUDED."first_assignment_at",
                             "canceled_at" = EXCLUDED."canceled_at";
                     `, values);
-                    }
-                    resolve();
-                } catch (err) {
-                    reject(err);
-                }
-            });
+            }
+            resolve();
+          } catch (err) {
+            reject(err);
+          }
+        });
     });
-}
+  }
 
-// Execute o processamento
-try {
+  // Execute o processamento
+  try {
     await processCSV();
     console.log('Dados inseridos com sucesso!');
-} catch (err) {
+  } catch (err) {
     console.error('Erro ao processar o CSV:', err);
-} finally {
+  } finally {
     // Encerre a conexão com o banco de dados
     await client.end();
-}
+  }
 
-// Função para deletar o CSV após o final
-fs.unlink(filePath, (err) => {
+  // Função para deletar o CSV após o final
+  fs.unlink(filePath, (err) => {
     if (err) {
       console.error(`Erro ao tentar deletar o arquivo: ${err}`);
     } else {
@@ -234,14 +240,11 @@ fs.unlink(filePath, (err) => {
     }
   });
 
-// Use o logger para mostrar a data e hora junto com a mensagem
-logger.info('Script executado');
+  // Use o logger para mostrar a data e hora junto com a mensagem
+  logger.info('Script executado');
 
 }
 
-import { scheduleJob } from "node-schedule";
-
-const job = scheduleJob('7 7-19 * * *', function(){
-
-  main()                              
+const job = scheduleJob('16,18,20,22,35,36,37 7-19 * * *', function () {
+  main()
 });
